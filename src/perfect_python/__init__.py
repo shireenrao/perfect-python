b03018cb415517dc47b03bed5b6a80b47d9928cc
Srinivas Nyayapati <shireenrao@gmail.com>
2021-09-23 11:43:38 -0400

    Initial commit

---
+++ src/perfect_python/__init__.py
@@ -0,0 +1,2 @@
+# src/perfect_python/__init__.py
+__version__ = "0.1.0"
---
+++ src/perfect_python/console.py
@@ -0,0 +1,26 @@
+import textwrap
+
+import click
+
+from . import __version__, wikipedia
+
+
+@click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
+@click.version_option(version=__version__)
+def main(language):
+    """The Perfect Python Project"""
+    data = wikipedia.random_page(language=language)
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
