a9f1cc2fb1a88636f7645980f7da71d3034c74da
Srinivas Nyayapati <shireenrao@gmail.com>
2021-09-24 13:10:08 -0400

    Add docstring to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""Package-wide test fixtures."""
 from unittest.mock import Mock
 
 from _pytest.config import Config
@@ -7,6 +8,7 @@ from pytest_mock import MockFixture
 
 @pytest.fixture
 def mock_requests_get(mocker: MockFixture) -> Mock:
+    """Pytest configuration hook."""
     mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Lorem Ipsum",
@@ -16,4 +18,5 @@ def mock_requests_get(mocker: MockFixture) -> Mock:
 
 
 def pytest_configure(config: Config) -> None:
+    """Fixture for mocking requests.get."""
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
